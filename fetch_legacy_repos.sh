e1f61b879d695f874ebb0ffdd853ca0db30e1a4a
Alban Grastien <alban.grastien@cea.fr>
2024-01-26 14:43:29 +0100

    Fix spaces in fetch + patch

--- fetch_legacy_repos.sh
+++ fetch_legacy_repos.sh
@@ -5,7 +5,7 @@ if [ ! -d src/legacy/prototree ]; then
   # Fetch ProtoTree repository
   git clone https://github.com/M-Nauta/ProtoTree src/legacy/prototree
   # Apply minor patch to improve reproducibility and ensure compatibility
-  git apply --directory src/legacy/prototree src/legacy/patches/prototree.patch
+  git apply --whitespace=fix --directory src/legacy/prototree src/legacy/patches/prototree.patch
   rm -rf src/legacy/prototree/.git
 fi
 
