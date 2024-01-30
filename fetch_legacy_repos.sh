3a13a6af463519e3980471ceb1fd4bf279e5981a
Romain Xu-Darme <romain.xu@protonmail.com>
2024-01-30 17:00:01 +0100

    Add ProtoPNet data augmentation for CUB200 in fetch_legacy_repos.sh

--- fetch_legacy_repos.sh
+++ fetch_legacy_repos.sh
@@ -9,6 +9,15 @@ if [ ! -d src/legacy/prototree ]; then
   rm -rf src/legacy/prototree/.git
 fi
 
+if [ ! -d src/legacy/protopnet ]; then
+  echo "Fetching ProtoPNet repository"
+  # Fetch ProtoPNet repository
+  git clone https://github.com/cfchen-duke/ProtoPNet src/legacy/protopnet
+  # Apply minor patch to improve reproducibility and ensure compatibility
+  git apply --whitespace=fix --directory src/legacy/protopnet src/legacy/patches/protopnet.patch
+  rm -rf src/legacy/protopnet/.git
+fi
+
 if [ ! -d data/CUB_200_2011/dataset ]; then
   echo "Downloading CUB-200-2011 dataset"
   mkdir -p data/CUB_200_2011
@@ -23,6 +32,12 @@ if [ ! -d data/CUB_200_2011/dataset ]; then
   msg=$(python3 src/legacy/prototree/preprocess_data/cub.py 1&> /dev/null)
   if [ $? != 0 ]; then
     echo $msg
+    exit
+  fi
+  echo "Performing ProtoPNet augmentation (this may take a while)"
+  msg=$(python3 src/legacy/protopnet/img_aug.py 1&> /dev/null)
+  if [ $? != 0 ]; then
+    echo "FAILED: ${msg}"
     exit
   fi
 fi
