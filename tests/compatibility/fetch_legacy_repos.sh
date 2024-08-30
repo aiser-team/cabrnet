261fd52879912df3ee37faabc43306b2e6e8ba7c
Romain Xu-Darme <romain.xu@protonmail.com>
2024-08-30 14:53:06 +0200

    Refactor script for downloading legacy repositories.

--- tests/compatibility/fetch_legacy_repos.sh
+++ tests/compatibility/fetch_legacy_repos.sh
@@ -4,26 +4,25 @@
 location_abs=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "${location_abs}"/../../ || exit
 
-if [ ! -d tests/compatibility/prototree/legacy ]; then
-  echo "Fetching ProtoTree repository"
-  # Fetch ProtoTree repository
-  git clone https://github.com/M-Nauta/ProtoTree tests/compatibility/prototree/legacy
-  rm -rf tests/compatibility/prototree/legacy/.git
-  # Convert everything to UNIX format
-  { find tests/compatibility/prototree/legacy -type f -print0 | xargs -0 dos2unix 2>&1; }> /dev/null
-  # Apply minor patch to improve reproducibility and ensure compatibility
-  patch -p1 -d tests/compatibility/prototree/legacy < tests/compatibility/prototree/prototree.patch
-fi
+declare -a legacy_repos=(
+  "ProtoPNet https://github.com/cfchen-duke/ProtoPNet"
+  "ProtoTree https://github.com/M-Nauta/ProtoTree"
+)
 
-if [ ! -d tests/compatibility/protopnet/legacy ]; then
-  echo "Fetching ProtoPNet repository"
-  # Fetch ProtoPNet repository
-  git clone https://github.com/cfchen-duke/ProtoPNet tests/compatibility/protopnet/legacy
-  rm -rf tests/compatibility/protopnet/legacy/.git
-  # Convert everything to UNIX format
-  { find tests/compatibility/protopnet/legacy -type f -print0 | xargs -0 dos2unix 2>&1; } > /dev/null
-  # Apply minor patch to improve reproducibility and ensure compatibility
-  patch -p1 -d tests/compatibility/protopnet/legacy < tests/compatibility/protopnet/protopnet.patch
-fi
+for entry in "${legacy_repos[@]}"; do
+  arch_name=$(echo "${entry}" | awk '{split($0,a," "); print a[1]}')
+  root_path="tests/compatibility/$(echo "${arch_name}" | awk '{print tolower($0)}')"
+  git_path=$(echo "${entry}" | awk '{split($0,a," "); print a[2]}')
+
+  if [ ! -d "${root_path}/legacy" ]; then
+    echo "Fetching ${arch_name} repository"
+    git clone "${git_path}" "${root_path}/legacy"
+    rm -rf "${root_path}/legacy/.git"
+    # Convert everything to UNIX format
+    { find "${root_path}/legacy" -type f -print0 | xargs -0 dos2unix 2>&1; }> /dev/null
+    # Apply minor patch to improve reproducibility and ensure compatibility
+    patch -p1 -d "${root_path}/legacy" < "${root_path}/corrections.patch"
+  fi
+done
 
 python3 tools/download_examples.py -t legacy_models
