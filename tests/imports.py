e6a033d355a2611a6069ab2392c768f2549653f3
Romain Xu-Darme <romain.xu@protonmail.com>
2026-01-30 14:40:24 +0100

    [HOTFIX] Fixes import test.

--- tests/imports.py
+++ tests/imports.py
@@ -10,25 +10,25 @@ class TestImports(unittest.TestCase):
         """
         Tests the import of cabrnet generic module.
         """
-        import cabrnet.generic
+        import cabrnet.archs.generic
 
     def test_cabrnet_protopnet(self):
         """
         Tests the import of cabrnet protopnet module.
         """
-        import cabrnet.protopnet
+        import cabrnet.archs.protopnet
 
     def test_cabrnet_prototree(self):
         """
         Tests the import of cabrnet prototree module.
         """
-        import cabrnet.prototree
+        import cabrnet.archs.prototree
 
     def test_cabrnet_utils(self):
         """
         Tests the import of cabrnet utils module.
         """
-        import cabrnet.utils
+        import cabrnet.core.utils
 
     def test_cabrnet_evaluation(self):
         """
@@ -40,4 +40,10 @@ class TestImports(unittest.TestCase):
         """
         Tests the import of cabrnet visualization module.
         """
-        import cabrnet.visualization
+        import cabrnet.core.visualization
+
+    def test_cabrnet_interface(self):
+        """
+        Tests the import of cabrnet visualization module.
+        """
+        import cabrnet.core.interface
